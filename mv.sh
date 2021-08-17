04f70c8dda5717768794076895aa735957535ccc
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-08-17 12:59:26 -0300

    Update mv.sh

--- mv.sh
+++ mv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www/html/_S_script/sshplus-v32
-mv delhost alterarsenha criarusuario expcleaner mudardata remover criarteste verifbot droplimiter alterarlimite ajuda sshmonitor badvpn userbackup instsqd blockt otimizar menu speedtest banner senharoot reiniciarservicos reiniciarsistema attscript conexao delscript detalhes botssh infousers verifatt limiter uexpired cabecalho bot open.py proxy.py wsproxy.py slow_dns /var/www/html/_S_script/
+mv addhost delhost alterarsenha criarusuario expcleaner mudardata remover criarteste verifbot droplimiter alterarlimite ajuda sshmonitor badvpn userbackup instsqd blockt otimizar menu speedtest banner senharoot reiniciarservicos reiniciarsistema attscript conexao delscript detalhes botssh infousers verifatt limiter uexpired cabecalho bot open.py proxy.py wsproxy.py slow_dns /var/www/html/_S_script/
