af1df03811d57531ff21123574199f59a2d78b32
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-08-17 12:38:59 -0300

    Create mv.sh

---
+++ mv.sh
@@ -0,0 +1,6 @@
+cd /var/www/html/_S_script/sshplus-v32
+_mdls=("addhost" "delhost" "alterarsenha" "criarusuario" "expcleaner" "mudardata" "remover" "criarteste" "verifbot" "droplimiter" "alterarlimite" "ajuda" "sshmonitor" "badvpn" "userbackup" "instsqd" "blockt" "otimizar" "menu" "speedtest" "banner" "senharoot" "reiniciarservicos" "reiniciarsistema" "attscript" "conexao" "delscript" "detalhes" "botssh" "infousers" "verifatt" "limiter" "uexpired" "cabecalho" "bot" "open.py" "proxy.py" "wsproxy.py" "slow_dns")
+for _arq in ${_mdls[@]}; do
+	[[ -e $_dir1/$_arq ]] && rm $_dir1/$_arq >/dev/null 2>&1
+	mv -c -P $_arq /var/www/html/_S_script/
+done
